bb67ea65c502f70992780e67a8bc923d5e0e6424
Evanilla57 <grigg005@cougars.csusm.edu>
2023-12-05 21:02:36 -0800

    added Cat model

---
+++ server/models/Cat.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+
+const catSchema = new Schema({
+    catName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    catAge: {
+        type: Number,
+        required: true,
+    },
+    catBreed: {
+        type: String,
+        required: true,
+    },
+    catBio: {
+        type: String,
+        required: true,
+        minlength: 10,
+    },
+});
+
+
+const Cat = model("Cat", catSchema);
+
+module.exports = Cat;
