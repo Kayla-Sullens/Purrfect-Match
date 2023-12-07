c968e45c8d35e9b130bc64cf7fffe157ca294d66
Evanilla57 <grigg005@cougars.csusm.edu>
2023-12-07 12:13:23 -0800

    added CatOne page

---
+++ client/src/pages/CatOne.jsx
@@ -0,0 +1,12 @@
+const CatOne = () => {
+    return (
+      <div className="container my-2">
+        <p>
+          Check out this one cat!
+        </p>
+      </div>
+    );
+  };
+  
+  export default CatOne;
+  
