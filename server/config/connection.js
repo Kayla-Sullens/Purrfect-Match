0582cbab644f0290fb2dd79fb022372492f8ce39
Evanilla57 <grigg005@cougars.csusm.edu>
2023-12-07 21:05:53 -0800

    updated mongoose connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb+srv://grigg005:GcHAI3zWq46VANCP@cluster0.f9gkys4.mongodb.net/"
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/purrfect-match"
 );
 
 module.exports = mongoose.connection;
