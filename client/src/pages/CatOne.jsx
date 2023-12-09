cab16f5f7c384f97b2b6f33441ec6e18796224b4
Stephany <136421961+splacito22@users.noreply.github.com>
2023-12-09 14:52:22 -0800

    updated queries to include cat. started CatOne page

--- client/src/pages/CatOne.jsx
+++ client/src/pages/CatOne.jsx
@@ -1,12 +1,27 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { QUERY_CAT } from "../utils/queries";
+
 const CatOne = () => {
-    return (
-      <div className="container my-2">
-        <p>
-          Check out this one cat!
-        </p>
-      </div>
-    );
-  };
-  
-  export default CatOne;
-  
+  const { catId } = useParams();
+
+  // Use Apollo Client or another method to fetch the cat data
+  const { loading, error, data } = useQuery(QUERY_CAT, {
+    variables: { _id: catId },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const cat = data.cat;
+
+  return (
+    <div className="container my-2">
+      <h2>{cat.catName}</h2>
+      <p>{cat.catBio}</p>
+      {/* Add more details as needed */}
+    </div>
+  );
+};
+
+export default CatOne;
