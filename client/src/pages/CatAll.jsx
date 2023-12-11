858eeda15f28bae94b2ceb2e0e379c66bb65450c
Kayla <sullenspartyof3@gmail.com>
2023-12-11 08:06:31 -0800

    Working on CatAll page

--- client/src/pages/CatAll.jsx
+++ client/src/pages/CatAll.jsx
@@ -1,31 +1,31 @@
 
-// import { useQuery } from "@apollo/client";
-// import { QUERY_CAT } from "../utils/queries";
+import { useQuery } from "@apollo/client";
+import { QUERY_CAT } from "../utils/queries";
 
-// const CatAll = () => {
+const CatAll = () => {
 
-//   const { loading, error, data } = useQuery(QUERY_CAT, {
+  const { loading, error, data } = useQuery(QUERY_CAT, {
    
-//   });
+  });
 
-//   if (loading) return <p>Loading...</p>;
-//   if (error) return <p>Error: {error.message}</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
 
-//   const cat = data.cat;
+  const cat = data.cat;
 
-//   return (
-//     <div className="container my-2">
-//       <h2>Check out our avialable cats!</h2>
-//       <h3>{cat.catName}</h3>
-//       <p>{cat.catAge}</p>
-//       <p>{cat.catGender}</p>
-//       <p>{cat.catSpecies}</p>
-//       <p>{cat.catBio}</p>
-//     </div>
-//   );
-// };
+  return (
+    <div className="container my-2">
+      <h2>Check out our avialable cats!</h2>
+      <h3>{cat.catName}</h3>
+      <p>{cat.catAge}</p>
+      <p>{cat.catGender}</p>
+      <p>{cat.catSpecies}</p>
+      <p>{cat.catBio}</p>
+    </div>
+  );
+};
 
-// export default CatAll;
+export default CatAll;
 
 
   
