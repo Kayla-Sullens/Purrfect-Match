99bb4c64b038b47971ba7a94dca52d521b358da6
Stephany <136421961+splacito22@users.noreply.github.com>
2023-12-11 20:15:14 -0800

    post merge commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
         element: <CatOne />,
       },
       {
-        path: "/cats",
+        path: "/catall",
         element: <CatAll />,
       },
     ],
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -31,27 +31,3 @@ export const QUERY_CAT = gql`
     }
   }
 `;
-
-export const QUERY_CATS = gql`
-query cats {
-  cats {
-    userId
-    catSpecies
-    catName
-    catBio
-    catAge
-    _id
-  }
-}
-`;
-
-export const QUERY_COMMENT = gql`
-  query comment($_id: ID!) {
-    comment(_id: $_id) {
-      _id
-      comInfo
-      userId
-      catId
-    }
-  }
-`;
