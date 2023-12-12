4d095949b1c60063988d3ac5be3846967bb41b17
Evanilla57 <136149681+Evanilla57@users.noreply.github.com>
2023-12-12 09:01:34 -0800

    Revert "Feature/design login page"

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,54 +2,44 @@ import Auth from "../utils/auth";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  // const showNavigation = () => {
-  //   if (Auth.loggedIn()) {
-  //     return (
-  //       <ul className="flex-row">
-  //         <li className="mx-1">
-  //           <Link to="/">Home</Link>
-  //         </li>
-  //         <li className="mx-1">
-  //           <a href="/" onClick={() => Auth.logout()}>
-  //             Logout
-  //           </a>
-  //         </li>
-  //       </ul>
-  //     );
-  //   }
+  const showNavigation = () => {
+    if (Auth.loggedIn()) {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="mx-1">
+            <Link href="/" onClick={() => Auth.logout()}>
+              Logout
+            </Link>
+          </li>
+        </ul>
+      );
+    }
 
-  //   return (
-  //     <div className="user_info">
-  //       <a href="/login">Sign In</a>
-  //       <span>|</span>
-  //       <a href="/signup">Sign Up</a>
-  //     </div>
-  //   );
-  // };
+    return (
+      <ul className="flex-row">
+        <li className="mx-1">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="mx-1">
+          <Link to="/signup">Signup</Link>
+        </li>
+
+        <li className="mx-1">
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    );
+  };
 
   return (
-    <header className="main_header">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <Link to="/"> <img src="./src/assets/logo.png" alt="" /></Link>
-          </div>
-          <div className="col-md-8">
-            <div className="head_nav">
-              <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Cat All</a></li>
-                <li><a href="">Cat One</a></li>
-              </ul>
-              <div className="user_info">
-                <a href="/login">Sign In</a>
-                <span>|</span>
-                <a href="/signup">Sign Up</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <header className="flex-row px-1">
+      <h1>
+        <Link to="/">Purrfect Match</Link>
+      </h1>
+      <nav>{showNavigation()}</nav>
     </header>
   );
 };
