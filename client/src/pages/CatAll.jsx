6194ee81736a9b2138d7386b0295d03199d518cd
Kayla <sullenspartyof3@gmail.com>
2023-12-11 18:32:01 -0800

    Available Cats page mostly done

--- client/src/pages/CatAll.jsx
+++ client/src/pages/CatAll.jsx
@@ -1,31 +1,32 @@
-
 import { useQuery } from "@apollo/client";
-import { QUERY_CAT } from "../utils/queries";
+import { QUERY_CATS } from "../utils/queries";
 
 const CatAll = () => {
-
-  const { loading, error, data } = useQuery(QUERY_CAT, {
-   
-  });
+  const { loading, error, data } = useQuery(QUERY_CATS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
-  const cat = data.cat;
+  // derrived state
+  const cats = data?.cats || [];
 
   return (
     <div className="container my-2">
       <h2>Check out our avialable cats!</h2>
-      <h3>{cat.catName}</h3>
-      <p>{cat.catAge}</p>
-      <p>{cat.catGender}</p>
-      <p>{cat.catSpecies}</p>
-      <p>{cat.catBio}</p>
+      {cats.map((cat) => (
+        <div key={cat._id}>
+          <h3>{cat.catName}</h3>
+          <div>
+          <img src={`/images/${cat.catImage}`}/>
+          </div>
+          <p>{cat.catAge}</p>
+          <p>{cat.catGender}</p>
+          <p>{cat.catSpecies}</p>
+          <p>{cat.catBio}</p>
+        </div>
+      ))}
     </div>
   );
 };
 
 export default CatAll;
-
-
-  
