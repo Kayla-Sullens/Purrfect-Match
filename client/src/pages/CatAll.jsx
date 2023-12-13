c738e2bf3e86f44d30683b9ae0333d68e3a1a3c3
Kayla <sullenspartyof3@gmail.com>
2023-12-12 16:06:08 -0800

    Fixing little things

--- client/src/pages/CatAll.jsx
+++ client/src/pages/CatAll.jsx
@@ -1,29 +1,31 @@
+import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { QUERY_CATS } from "../utils/queries";
 
 const CatAll = () => {
-
   const { loading, error, data } = useQuery(QUERY_CATS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
-  const cat = data.cat;
+  // const cat = data.cat;
   // derrived state
   const cats = data?.cats || [];
 
   return (
     <div className="container my-2">
       <h2>Check out our avialable cats!</h2>
-      {cats.map((cat) => (
+      {cats.map((cat, idx) => (
         <div key={cat._id}>
-          <h3>{cat.catName}</h3>
+          <Link to={`/catone/${cat._id}/${idx}`}>
+            <h3>{cat.catName}</h3>
+          </Link>
           <div>
-          <img src={`/images/${cat.catImage}`}/>
+            <img src={`/images/${cat.catImage}`} />
           </div>
           <p>Age: {cat.catAge}</p>
-          <p>{cat.catGender}</p>
-          <p>{cat.catSpecies}</p>
+          <p>Gender: {cat.catGender}</p>
+          <p>Species: {cat.catSpecies}</p>
           <p>{cat.catBio}</p>
         </div>
       ))}
